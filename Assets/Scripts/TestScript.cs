ee3a18d79ffd1ff632fe104f14d6e45da648dd4e
hermes <dreamcastrar@gmail.com>
2024-12-25 21:03:24 +0900

    First Test
    fo-dicom 설치, 테스트 스크립트 생성

---
+++ Assets/Scripts/TestScript.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FellowOakDicom;
+
+public class TestScript : MonoBehaviour
+{
+    
+    void Start()
+    {
+        //Create test
+        DicomFile file = new DicomFile();
+    }
+}
